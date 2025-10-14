f8801705912dc4ac69213dec64fac539f14fb683
nghiallu <nghia.lu@gleads.vn>
2025-10-14 15:04:24 +0700

    first commit

---
+++ resources/js/pages/auth/verify-email.tsx
@@ -0,0 +1,48 @@
+// Components
+import EmailVerificationNotificationController from '@/actions/App/Http/Controllers/Auth/EmailVerificationNotificationController';
+import { logout } from '@/routes';
+import { Form, Head } from '@inertiajs/react';
+
+import TextLink from '@/components/text-link';
+import { Button } from '@/components/ui/button';
+import { Spinner } from '@/components/ui/spinner';
+import AuthLayout from '@/layouts/auth-layout';
+
+export default function VerifyEmail({ status }: { status?: string }) {
+    return (
+        <AuthLayout
+            title="Verify email"
+            description="Please verify your email address by clicking on the link we just emailed to you."
+        >
+            <Head title="Email verification" />
+
+            {status === 'verification-link-sent' && (
+                <div className="mb-4 text-center text-sm font-medium text-green-600">
+                    A new verification link has been sent to the email address
+                    you provided during registration.
+                </div>
+            )}
+
+            <Form
+                {...EmailVerificationNotificationController.store.form()}
+                className="space-y-6 text-center"
+            >
+                {({ processing }) => (
+                    <>
+                        <Button disabled={processing} variant="secondary">
+                            {processing && <Spinner />}
+                            Resend verification email
+                        </Button>
+
+                        <TextLink
+                            href={logout()}
+                            className="mx-auto block text-sm"
+                        >
+                            Log out
+                        </TextLink>
+                    </>
+                )}
+            </Form>
+        </AuthLayout>
+    );
+}
